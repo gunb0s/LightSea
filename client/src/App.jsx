5a45ec2e45fd82b07334b6159672e954ad4e6a0b
gunbos1031 <gunbos1031@gmail.com>
2022-04-09 18:31:34 +0900

    Implement NFTCard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import Filter from "./components/Filter/Filter";
 import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
+import NFTCardList from "./components/NFTCardList";
 
 const App = () => {
   return (
     <div>
       <Navigation />
-      <Filter />
+      <div style={{ display: "flex" }}>
+        <Filter />
+        <NFTCardList />
+      </div>
       <Footer />
     </div>
   );
